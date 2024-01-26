fa3e8461a0ea82d24cdfdad65d9ccbbcae870e65
ashuhitman <ashu8306@gmail.com>
2024-01-26 18:09:35 +0530

    profile page footer issue

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "https://phenomenal-pika-0a769a.netlify.app",
+      "http://192.168.1.2:5173",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
