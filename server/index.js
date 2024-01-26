2e7244e4fcc7a7b55d90ac7da808a51ad6b44476
ashuhitman <ashu8306@gmail.com>
2024-01-26 18:32:56 +0530

    permit for  patch on cors

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(
       "http://192.168.1.2:5173",
     ],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   })
 );
 app.use(express.json({ limit: "30mb", extended: true }));
