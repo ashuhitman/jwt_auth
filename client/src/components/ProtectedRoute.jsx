505a0e5510b9872122f2cba66ea5d896987304ff
ashuhitman <ashu8306@gmail.com>
2024-01-26 15:13:21 +0530

    added loader in protected route

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "./context/authContext";
+import Loader from "./Loader/Loader";
 
 function ProtectedRoute() {
   const { loading, token } = useAuth();
 
   // If the authentication is still loading, you can optionally show a loading indicator.
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <div
+        style={{
+          position: absolute,
+          inset: 0,
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Loader display="visible" />
+      </div>
+    );
   }
 
   // If there is no token (user is not authenticated), redirect to the login page.
