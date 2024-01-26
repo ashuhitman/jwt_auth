90bbad2f294da3b5f34322490111e1e15a220754
ashuhitman <ashu8306@gmail.com>
2024-01-26 18:02:52 +0530

    added alert for logout

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,16 +3,30 @@ import "./Header.scss";
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/authContext";
 import useNetwork from "../../hooks/useNetwork";
+import { alertStatus, useAlert } from "../context/AlertContext";
 
 function Header() {
   const { user, token, setToken } = useAuth();
   const { handleRequest, loading } = useNetwork();
+  const { setAlertStateFromComponent } = useAlert();
   const logout = async () => {
     try {
-      await handleRequest("/auth/logout", "DELETE", null, true);
+      setAlertStateFromComponent({
+        message: "logging out...",
+        status: alertStatus.SUCCESS,
+      });
+      const result = await handleRequest("/auth/logout", "DELETE", null, true);
       setToken(null);
+      setAlertStateFromComponent({
+        message: result.message,
+        status: alertStatus.SUCCESS,
+      });
     } catch (error) {
       console.log("logout error: ", error);
+      setAlertStateFromComponent({
+        message: error.message,
+        status: alertStatus.ERROR,
+      });
     }
   };
   return (
